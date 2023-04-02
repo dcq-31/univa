5aa8a50f30ea7f35dfd6aea5de4c570eb7c53570
dcq <dariancapoteq@gmail.com>
2023-04-02 18:34:33 -0400

    Create in home page course and about sections

--- src/types/component.ts
+++ src/types/component.ts
@@ -3,3 +3,15 @@
  */
 
 export type TLinkBase = string | { name: string }
+
+export interface IIcon {
+  name: string
+  svg: string
+}
+
+export interface ICourseVideo {
+  date: string
+  author: string
+  title: string
+  description: string
+}
