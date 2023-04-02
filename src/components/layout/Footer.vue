fcb4d2c03a97f8e029be525cc250944fd2173fd7
dcq <dariancapoteq@gmail.com>
2023-04-02 18:33:10 -0400

    Create main footer

---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+import { RouterLink } from 'vue-router'
+</script>
+<template>
+  <!-- ====== Footer Section Start -->
+  <footer
+    class="relative z-10 bg-cover py-10 lg:pt-16"
+    style="background-image: url('/footer-bg.jpg')"
+  >
+    <div class="container mx-auto">
+      <div class="-mx-4 flex flex-wrap">
+        <div class="w-full px-4 lg:w-1/2">
+          <div class="mb-10 w-full">
+            <RouterLink to="/#hero" class="mb-6 inline-block max-w-[230px]">
+              <img src="@/assets/images/univa-logo-white.png" alt="logo" class="max-w-full" />
+            </RouterLink>
+            <p class="mb-7 text-lg text-[#f3f4fe]">
+              We create digital experiences for brands and companies by using technology. <br />
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius neque eum autem dolorum
+              quidem architecto nesciunt?
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!-- ====== Footer Section End -->
+</template>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import Navbar from '@/components/layout/Navbar.vue'
+import Footer from '@/components/layout/Footer.vue'
 </script>
 
 <template>
   <div>
     <Navbar></Navbar>
     <RouterView></RouterView>
+    <Footer></Footer>
   </div>
 </template>
