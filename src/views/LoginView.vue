7285b0003b69470716242d5e761fa694e9ec5dbf
dcq <dariancapoteq@gmail.com>
2023-04-03 19:12:16 -0400

    Handle input forms

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,32 +1,37 @@
 <script lang="ts" setup>
+import { ref } from 'vue'
 import { RouterLink } from 'vue-router'
+const user = ref('')
+const password = ref('')
 </script>
 <template>
-  <div class="bg-neutral-100 py-4 px-8">
+  <div class="bg-neutral-100 py-2 px-5">
     <!-- ====== Forms Section Start -->
-    <section class="bg-[#F4F7FF] py-14 lg:py-20">
+    <section class="bg-[#F4F7FF] py-8 lg:py-10">
       <div class="container">
         <div class="-mx-4 flex flex-wrap">
           <div class="w-full px-4">
             <div
-              class="relative mx-auto max-w-[525px] overflow-hidden rounded-lg bg-white py-14 px-8 text-center sm:px-12 md:px-[60px]"
+              class="relative mx-auto max-w-[500px] overflow-hidden rounded-lg bg-white py-10 px-5 text-center sm:px-9 md:px-[40px]"
             >
-              <div class="mb-10 text-center">
-                <RouterLink to="/#hero" class="mx-auto inline-block max-w-[230px]">
+              <div class="mb-6 text-center">
+                <RouterLink to="/" class="mx-auto inline-block max-w-[240px]">
                   <img src="@/assets/images/univa-logo.png" alt="logo" />
                 </RouterLink>
               </div>
-
+              <div class="mb-5 text-left text-3xl font-semibold text-neutral-700">Login</div>
               <form>
                 <div class="mb-6">
                   <input
-                    type="email"
-                    placeholder="Email"
+                    v-model="user"
+                    type="text"
+                    placeholder="User"
                     class="bordder-[#E9EDF4] text-body-color w-full rounded-md border bg-[#FCFDFE] py-3 px-5 text-base placeholder-[#ACB6BE] outline-none transition focus:border-primary focus-visible:shadow-none"
                   />
                 </div>
                 <div class="mb-6">
                   <input
+                    v-model="password"
                     type="password"
                     placeholder="Password"
                     class="bordder-[#E9EDF4] text-body-color w-full rounded-md border bg-[#FCFDFE] py-3 px-5 text-base placeholder-[#ACB6BE] outline-none transition focus:border-primary focus-visible:shadow-none"
@@ -40,13 +45,15 @@ import { RouterLink } from 'vue-router'
                   />
                 </div>
               </form>
-
-              <RouterLink
-                to="/"
-                class="mb-2 inline-block text-base text-[#adadad] hover:text-primary"
-              >
-                Forget Password?
-              </RouterLink>
+              <div class="text-[#adadad]">
+                If you don't have any account,
+                <RouterLink
+                  to="/"
+                  class="mb-2 inline-block text-base text-primary-300 hover:text-primary"
+                >
+                  please register here.
+                </RouterLink>
+              </div>
             </div>
           </div>
         </div>
